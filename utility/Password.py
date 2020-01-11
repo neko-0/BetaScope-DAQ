4bcf237b42b6bde6f1961bf1dfd75c0ceafd3925
yuzhan <yuzhan.physics@gmail.com>
2020-01-10 17:58:23 -0800

    fixed encoding issue

--- utility/Password.py
+++ utility/Password.py
@@ -28,9 +28,9 @@ class Password:
             )
             if os.path.exists(user_pw_file):
                 with open(user_pw_file, "rb") as f:
-                    pw = getpass.getpass("Please enter your password: \n")
+                    pw = getpass.getpass("Please enter your password: \n").encode()
                     decode_pw = sha256(pw.rstrip()).hexdigest()
-                    stored_pw = f.read()
+                    stored_pw = f.read().decode()
                     if decode_pw == stored_pw:
                         print("Okay")
                         return (user, True)
