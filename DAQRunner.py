74a27bfe5bc09338ca5a1a0b0c9a1a5b17a096f5
yuzhan <yuzhan.physics@gmail.com>
2020-01-28 12:48:07 -0800

    change logger output format

--- DAQRunner.py
+++ DAQRunner.py
@@ -1,6 +1,10 @@
 import logging, coloredlogs
 
-logging.basicConfig(filename="/tmp/beta_daq.log", filemode="a")
+logging.basicConfig(
+    filename="/tmp/beta_daq.log",
+    filemode="a",
+    format="[%(asctime)s] [%(threadName)s] [%(name)s] [%(levelname)s]: %(message)s",
+)
 log = logging.getLogger(__name__)
 coloredlogs.install(level="DEBUG", logger=log)
 
