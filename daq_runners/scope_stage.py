eec9e2d5e8559e90c0be3f1367892bcd4c77d200
neko-0 <yuzhan.physics@gmail.com>
2024-03-01 08:36:54 -0800

    update scope stage

--- daq_runners/scope_stage.py
+++ daq_runners/scope_stage.py
@@ -9,7 +9,6 @@ import h5py
 import argparse
 import betascopedaq
 import tqdm
-import plotext as plt
 import numpy as np
 import logging
 
@@ -22,13 +21,15 @@ logger.setLevel(logging.INFO)
 def dry_test():
     pass
 
+
 def stage_stepper(x_start, x_end, x_step, y_start, y_end, y_step):
     x_incr = (x_end - x_start) / x_step
     y_incr = (y_end - y_start) / y_step
     for x in np.arange(x_start, x_end + x_incr, x_incr):
         for y in np.arange(y_start, y_end + y_incr, y_incr):
             yield x, y
-            
+
+
 def scope_stage(config_file):
 
     # read in JSON configuration file
@@ -44,12 +45,12 @@ def scope_stage(config_file):
 
     if not scope.initialize(config["ip_address"], config["trigger_setting"]):
         raise RuntimeError("Cannot connect to scope!")
-    
+
     # connect to stage. Need a default fallback if no stage?
     stage = None
     stage_steps = None
     if not config["stage"]["manual"]:
-        axes = {"x" : config["stage"]["x_axis"], "y" : config["stage"]["y_axis"]}
+        axes = {"x": config["stage"]["x_axis"], "y": config["stage"]["y_axis"]}
         stage = betascopedaq.Stage(axes)
         stage.setup_api(config["stage"]["api"])
         stage.connect()
@@ -86,11 +87,16 @@ def scope_stage(config_file):
     timestamp = time.time()  # just initialize it with staring time
     wait_timeout = 10
 
+    # setting pos to starting points
+    stage.to2d(config["stage"]["x_start"], config["stage"]["x_end"], True)
+
     # running waveform acquisition
     for i, (xpos, ypos) in enumerate(stage_steps):
         stage.to2d(xpos, ypos, True)
         with h5py.File(f"{output_path}/{output_name}_{i}.hdf5", "a") as output_file:
-            pbar = tqdm.tqdm(total=int(nevents * nsegments), unit="evt", dynamic_ncols=True)
+            pbar = tqdm.tqdm(
+                total=int(nevents * nsegments), unit="evt", dynamic_ncols=True
+            )
             i_event = 0
             while i_event < nevents:
                 trig_status = None
@@ -107,11 +113,11 @@ def scope_stage(config_file):
                     print("error in getting waveform")
                     scope.reset()
                     continue
-                    
+
                 # storing the waveforms
                 evt_grp = output_file.create_group(f"evt{i_event}")
-                evt_grp.attrs['xpos'] = xpos
-                evt_grp.attrs['ypos'] = ypos
+                evt_grp.attrs["xpos"] = xpos
+                evt_grp.attrs["ypos"] = ypos
                 for channel, wav_seg in waveforms.items():
                     for iseg, wav in enumerate(wav_seg):
                         seg_index = f"seg{iseg}"
@@ -126,6 +132,9 @@ def scope_stage(config_file):
 
                 pbar.update(nsegments)
                 i_event += 1
+
+    # reset pos to starting points
+    stage.to2d(config["stage"]["x_start"], config["stage"]["x_end"], True)
 
 
 if __name__ == "__main__":
